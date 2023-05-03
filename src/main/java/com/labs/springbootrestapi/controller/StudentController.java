bdc953fc5206d46e833a0e0b68e746592a9cc38f
hadipsy27 <hadipsy27@gmail.com>
2023-05-03 11:40:28 +0700

    Update student controller base url

--- src/main/java/com/labs/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/labs/springbootrestapi/controller/StudentController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/students")
 public class StudentController {
 
 //    http://localhost:8080/student
@@ -26,7 +27,7 @@ public class StudentController {
 
 
     /* http://localhost:8080/students */
-    @GetMapping("/students")
+    @GetMapping
     public ResponseEntity<List<Student>> getStudents(){
         List<Student> students = new ArrayList<>();
         students.add(new Student(1, "John", "Doe"));
@@ -38,9 +39,9 @@ public class StudentController {
 
     /* Springboot REST API with path variable
      * {id} -Uri Template Variable
-     * http://localhost:8080/student/1/John/Doe
+     * http://localhost:8080/students/1/John/Doe
      * */
-    @GetMapping("/student/{id}/{first-name}/{last-name}")
+    @GetMapping("/{id}/{first-name}/{last-name}")
     public ResponseEntity<Student> studentPathVariable(@PathVariable("id") int studentId,
                                        @PathVariable("first-name") String firstName,
                                        @PathVariable("last-name") String lastName) {
@@ -51,7 +52,7 @@ public class StudentController {
     /* Springboot REST API with Request Param
      * http://localhost:8080/student/query?studentId=1&firstName=John&lastName=Doe
      */
-    @GetMapping("/student/query")
+    @GetMapping("/query")
     public ResponseEntity<Student> studentRequestVariable(@RequestParam int studentId,
                                           @RequestParam String firstName,
                                           @RequestParam String lastName) {
@@ -63,8 +64,7 @@ public class StudentController {
      * @PostMapping and @RequestBody
      * http://localhost:8080/student/create
      * */
-    @PostMapping("/students/create")
-//    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/create")
     public ResponseEntity<Student> createStudent(@RequestBody Student student) {
         System.out.println(student.getId());
         System.out.println(student.getFirstName());
@@ -76,7 +76,7 @@ public class StudentController {
      * Updating Existing Request
      * http://localhost:8080/student/1/update
      * */
-    @PutMapping("students/{id}/update")
+    @PutMapping("/{id}/update")
     public ResponseEntity<Student> updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
@@ -87,7 +87,7 @@ public class StudentController {
      * Delete Existing Request
      * http://localhost:8080/student/1/delete
      * */
-    @DeleteMapping("/students/{id}/delete")
+    @DeleteMapping("/{id}/delete")
     public ResponseEntity<String> deleteStudent(@PathVariable("id") int studentId){
         System.out.println(studentId);
         return ResponseEntity.ok("Delete Successfully");
